053711005c60f890d18d8652b0fcd8344d607ae3
Vivek Shadija <vivekshadija@gmail.com>
2022-03-20 17:39:12 +0530

    Message

---
+++ script.js
@@ -0,0 +1,12 @@
+function initMethod() {
+    changeHeight();
+}
+
+function changeHeight() {
+    var height = Math.floor(Math.random() * 31) + 'em';
+    water = document.getElementById('water');
+    water.style.height = height;
+    console.log(height);
+    setTimeout(changeHeight, 3000);
+}
+//max height  = 27 em;
